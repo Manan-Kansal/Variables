a5fbec2d4e06a37e8202063b0eaf5e71969f790d
Manan-Kansal <142139855+Manan-Kansal@users.noreply.github.com>
2023-08-16 18:47:45 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,20 @@
+function def1(){
+    var def_of_variables = "Variable is a name of storage location";
+    document.getElementById('definition1').innerHTML = def_of_variables;
+}
+function types(){
+    var types_of_variables = "Var , Let , Const"
+    document.getElementById('typesOfVariables').innerHTML = types_of_variables;
+}
+function def2(){
+    var def_of_var = "VAR declares a variable . It can be redeclared and the values can be also reassigned.";
+    document.getElementById('definition2').innerHTML = def_of_var;
+}
+function def3(){
+    var def_of_let = "The keyword let in Javascript is used to declare a variable that is block scoped in JavaScript.";
+    document.getElementById('definition3').innerHTML = def_of_let;
+}
+function def4(){
+    var def_of_const = "Const (constant) in programming is a keyword that defines a variable or pointer as unchangeable";
+    document.getElementById('definition4').innerHTML = def_of_const;
+}
